e228116c043f09421204045d27eeeda5b2a853c3
Rutvik-C <rutvikchoudhari97@gmail.com>
2022-01-30 01:43:51 +0530

    Features added

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-
-from . import views
-from .utils_text_summarizing import speech_to_text
 from .views import *
-# from .views_to_dos import *
-# from .view_pomodoro import *
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", home, name="home"),
     
     path('todo/', list_todos, name="todo"),
     path('todo/<int:pk>/', detailed_todos, name="detailed_todos"),
@@ -16,20 +11,20 @@ urlpatterns = [
     path('edit_todo/<int:id>', edit_todo, name="edit_todo"),
     path('handle_todo_done/<int:pk>/', handle_todo_done, name="handle_todo_done"),
     
-    path('journal/', views.journal, name='journal'),
-    path('detailed_journal/<int:id>/', views.detailed_journal, name='detailed_journal'),
+    path('journal/', journal, name='journal'),
+    path('detailed_journal/<int:id>/', detailed_journal, name='detailed_journal'),
     
     path('summarize_text/', speech_to_text, name="summarize_text"),
     
-    path('health/', views.health, name="health"),
-    path('meditation/', views.meditation, name="meditation"),
+    path('health/', health, name="health"),
+    path('meditation/', meditation, name="meditation"),
     
     path('pomodoro/', start_pomodoro_timer, name="pomodoro"),
     path('break_session/', break_session, name="break_session"),
     path('relaxing_sounds/', relaxing_sounds, name="relaxing_sounds"),
     path('play_games/', play_games, name="play_games"),
     
-    path('logout/', views.signout, name="logout"),
-    path('login/', views.login_user, name="login"),
-    path('signup/', views.signup, name="signup"),
+    path('logout/', signout, name="logout"),
+    path('login/', login_user, name="login"),
+    path('signup/', signup, name="signup"),
 ]
